a2e792bbc40530509e9caf1e2df4cbe62a90d2c1
itodaisuke <itodaisuke@fuji.waseda.jp>
2024-08-19 16:47:06 +0900

    tmp commit 0819

--- eipl/tutorials/robosuite/sarnn/bin/train.py
+++ eipl/tutorials/robosuite/sarnn/bin/train.py
@@ -22,6 +22,12 @@ from eipl.utils import EarlyStopping, check_args, set_logdir, normalization
 sys.path.append("./libs/")
 from fullBPTT import fullBPTTtrainer
 
+#0819追加
+import glob
+import cv2
+import pickle
+import torch.nn as nn
+
 # argument parser
 parser = argparse.ArgumentParser(
     description="Learning spatial autoencoder with recurrent neural network"
@@ -61,12 +67,64 @@ else:
 
 # load dataset
 minmax = [args.vmin, args.vmax]
+"""
 images_raw = np.load("../simulator/data/train/images.npy")
 joints_raw = np.load("../simulator/data/train/joints.npy")
 joint_bounds = np.load("../simulator/data/joint_bounds.npy")
 images = normalization(images_raw.transpose(0, 1, 4, 2, 3), (0, 255), minmax)
 joints = normalization(joints_raw, joint_bounds, minmax)
-train_dataset = MultimodalDataset(images, joints, device=device, stdev=stdev)
+"""
+
+#0819追加
+#画像
+data_dir = "/home/ogata/workspace/ito/d3il/environments/dataset/data/aligning/all_data/"
+bp_images = []
+bp_imgs = glob.glob(data_dir + 'images/bp-cam/*/*.jpg')
+data_length = 10
+#bp_imgs.sort(key=lambda x: int(os.path.basename(x).split('.')[0]))
+
+counter = 0
+for img in bp_imgs:
+    counter +=1
+    if counter ==data_length:
+        print("img",img)
+        break
+    image = cv2.imread(img).astype(np.float32)
+    image = image.transpose((2, 0, 1)) / 255.
+
+    #image = torch.from_numpy(image).to(device).float().unsqueeze(0)
+
+    bp_images.append(image)
+
+
+#関節角度
+state_list = glob.glob(data_dir+"state/*.pkl")
+print(len(state_list))
+robot_state_list = []
+
+for i in range(len(state_list)):
+    path = state_list[i]
+    if i == data_length:
+        break
+    with open(path, 'rb') as f:
+        env_state = pickle.load(f)
+        robot_joint_state = torch.from_numpy(env_state['robot']['j_pos'])
+    robot_state_list.append(robot_joint_state)
+
+print(len(robot_state_list))
+
+print("Dataset Loaded!!")
+print("bp_images",len(bp_images))
+print("robot_state_list",len(robot_state_list))
+#assert False
+
+
+#長さが違うので0padding
+#self.data_length = 
+a = nn.utils.rnn.pack_sequence(robot_state_list, enforce_sorted=False)
+robot_state_list, _ = nn.utils.rnn.pad_packed_sequence(a,batch_first =True,padding_value=0)
+
+train_dataset = MultimodalDataset(bp_images, robot_state_list, device=device, stdev=stdev)
 train_loader = torch.utils.data.DataLoader(
     train_dataset,
     batch_size=args.batch_size,
